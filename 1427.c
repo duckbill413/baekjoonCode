84ccca882bfe853f834eabe9a5b618b6f402ba72
grammaCode <86183856+grammaCode@users.noreply.github.com>
2021-06-25 17:15:28 +0900

    1427 추가

---
+++ 1427.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    int array[10] = {0};
+    int sum = 0;
+    int i = 0;
+    while (num != 0)
+    {
+        array[i] = num % 10;
+        num /= 10;
+        i++;
+    }
+    for (int j = 0; j < i - 1; j++)
+    {
+        for (int k = 0; k < i - j - 1; k++)
+        {
+            if (array[k] > array[k + 1])
+            {
+                int tmp = array[k + 1];
+                array[k + 1] = array[k];
+                array[k] = tmp;
+            }
+        }
+    }
+    int multi = 1;
+    for (int j = 0; j < i; j++)
+    {
+        sum += multi * array[j];
+        multi *= 10;
+    }
+    printf("%d", sum);
+    return 0;
+}`
---
+++ 2751.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+//수 정렬하기(오름차순)
+//버블정렬
+int main()
+{
+    int cnt;
+    scanf("%d", &cnt);
+    int num[10000] = {0};
+    for (int i = 0; i < cnt; i++)
+        scanf("%d", &num[i]);
+
+    for (int i = 0; i < cnt - 1; i++)
+    {
+        for (int j = 0; j < cnt - i - 1; j++)
+        {
+            if (num[j] > num[j + 1])
+            {
+                int tmp = num[j + 1];
+                num[j + 1] = num[j];
+                num[j] = tmp;
+            }
+        }
+    }
+
+    for (int i = 0; i < cnt; i++)
+        printf("%d\n", num[i]);
+
+    return 0;
+}
