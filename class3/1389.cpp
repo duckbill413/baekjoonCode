4d2818f5835bf273e3ad50c2e070d3bafed55aa0
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-01-16 00:34:34 +0900

    케빈 베이컨의 6단계 법칙

--- class3/1389.cpp
+++ class3/1389.cpp
@@ -33,48 +33,58 @@ int main()
         sort(data[i].begin(), data[i].end());
         data[i].erase(unique(data[i].begin(), data[i].end()), data[i].end());
     }
-    cout << endl;
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 0; j < data[i].size(); j++)
-            cout << data[i][j] << " ";
-        cout << endl;
-    }
 
     for (int i = 1; i <= N; i++)
-    {
         bfs(i, data, visit[i]);
-    }
 
+    int *sum = new int[N + 1]();
     for (int i = 1; i <= N; i++)
     {
         for (int j = 1; j <= N; j++)
-            cout << visit[i][j] << " ";
-        cout << endl;
+        {
+            // cout << visit[i][j] << " ";
+            sum[i] += visit[i][j];
+        }
+        // cout << "  sum: " << sum[i];
+        // cout << endl;
     }
+    int min, minIdx;
+    for (int i = 1; i <= N; i++)
+    {
+        if (min > sum[i] || i == 1)
+        {
+            min = sum[i];
+            minIdx = i;
+        }
+    }
+    cout << minIdx << endl;
+
     delete[] data;
     for (int i = 0; i < N + 1; i++)
         delete[] visit[i];
     delete[] visit;
+    delete[] sum;
 }
 void bfs(int start, vector<int> *data, int *visit)
 {
-    int cnt = 0;
+    int maxRange = 0;
+    int Range = 0;
     queue<int> q;
     q.push(start);
     visit[start] = 0;
+
     while (!q.empty())
     {
         int n = q.front();
         q.pop();
-        cnt++;
+
         for (int i = 0; i < data[n].size(); i++)
         {
             int nn = data[n][i];
-            if (visit[nn] == 0)
+            if (visit[nn] == 0 && nn != start)
             {
                 q.push(nn);
-                visit[nn] = cnt;
+                visit[nn] = visit[n] + 1;
             }
         }
     }
