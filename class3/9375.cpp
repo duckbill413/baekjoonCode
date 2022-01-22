11e9a98a6e88f06c9cac27cda66fae3d4890b3b1
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-01-23 00:40:26 +0900

    패션왕 신해빈

---
+++ class3/9375.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T;
+    cin >> T;
+
+    int n;
+    string cloth, kind;
+    while (T--)
+    {
+        map<string, int> map;
+        cin >> n;
+
+        while (n--)
+        {
+            cin >> cloth >> kind;
+            if (map.find(kind) == map.end()) //해당 종류의 옷이 존재하지 않을 경우
+                map.insert({kind, 1});
+            else
+                map[kind]++;
+        }
+
+        int ans = 1;
+        for (auto i : map)
+        {
+            cout << "map: " << i.second << '\n';
+            ans *= (i.second + 1);
+        }
+        ans--;
+        cout << ans << '\n';
+    }
+}
