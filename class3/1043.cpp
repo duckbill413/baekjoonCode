edee98239eaa07bcf7eca961d2419a623b271725
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-01-28 04:09:17 +0900

    거짓말

--- class3/1043.cpp
+++ class3/1043.cpp
@@ -1,3 +1,4 @@
+// 1043 거짓말
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -13,14 +14,20 @@ int main()
     int N, M;
     cin >> N >> M;
 
-    vector<int> trueth(N);
-    vector<vector<int>> party(M + 1, vector<int>());
+    //진실을 아는 자
+    vector<int> truth(N + 1, 0);
+    int knowTrue;
+    cin >> knowTrue;
+    for (int i = 0; i < knowTrue; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        truth[tmp] = 1;
+    }
 
+    vector<vector<int>> party(M + 1, vector<int>()); //파티원 벡터
+    //파티원 입력
     int num;
-    cin >> num;
-    for (int i = 0; i < num; i++)
-        cin >> trueth[i];
-
     for (int i = 1; i <= M; i++)
     {
         cin >> num;
@@ -30,6 +37,38 @@ int main()
             cin >> tmp;
             party[i].push_back(tmp);
         }
-        sort(party[i].begin(), party[i].end());
     }
+    //진실을 아는 파티원 찾기
+    //파티원 중에 진실을 아는 인원이 있을 경우 다른 인원 들도 진실을 알게 되므로 진실을 아는 인원에 추가하여 다시 검색해야한다.
+    vector<int> flag(M + 1, 0); //한번 진실을 아는 인원을 찾은 경우는 다시 검색할 필요가 없다.
+    for (int i = 1; i <= M; i++)
+    {
+        if (flag[i] == 1)
+            continue;
+        for (int j = 0; j < party[i].size(); j++)
+        {
+            if (truth[party[i][j]] == 1)
+            {
+                for (int k = 0; k < party[i].size(); k++)
+                    truth[party[i][k]] = 1;
+                flag[i] = 1;
+                i = 0; //처음부터 재검색
+                break;
+            }
+        }
+    }
+    int cnt = 0; //진실을 아는 사람이 포함된 파티의 수
+    for (int i = 1; i <= M; i++)
+    {
+        for (int j = 0; j < party[i].size(); j++)
+        {
+            if (truth[party[i][j]] == 1)
+            {
+                cnt++;
+                break;
+            }
+        }
+    }
+
+    cout << M - cnt; //진실을 모르는 파티
 }
