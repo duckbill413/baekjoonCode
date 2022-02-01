eaa4375c3d2757b2baf3394b58a3c1b5c9e46936
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-02-01 23:32:19 +0900

    웜홀

--- class4/1865.cpp
+++ class4/1865.cpp
@@ -3,14 +3,16 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
-#define INF 1e9
+#define INF 987654321
 using namespace std;
 typedef tuple<int, int, int> T;
 bool calcReturnTime(int N, int M, int W);
-void TimeInit(vector<int> &TIME);
-void BellmanFord(int start, vector<T> &MAP, vector<int> &TIME);
+bool BellmanFord(vector<T> &MAP, vector<int> &TIME);
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     int TC;
     int N, M, W;
     cin >> TC;
@@ -26,7 +28,7 @@ int main()
 bool calcReturnTime(int N, int M, int W)
 {
     vector<T> MAP;
-    vector<int> TIME(N + 1, INF);
+    vector<int> TIME(N + 1, 0);
     int S, E, T;
     for (int i = 0; i < M; i++)
     {
@@ -34,29 +36,39 @@ bool calcReturnTime(int N, int M, int W)
         MAP.push_back({S, E, T});
         MAP.push_back({E, S, T});
     }
-    for (int i = 0; i < M; i++)
+    for (int i = 0; i < W; i++)
     {
         cin >> S >> E >> T;
         MAP.push_back({S, E, -T});
     }
+
+    return BellmanFord(MAP, TIME);
 }
 
-void BellmanFord(int start, vector<T> &MAP, vector<int> &TIME)
+bool BellmanFord(vector<T> &MAP, vector<int> &TIME)
 {
     int N = TIME.size() - 1;
-    TIME[1] = 0;
-
     for (int i = 1; i <= N - 1; i++)
     {
         for (int j = 0; j < MAP.size(); j++)
         {
-            T cur = MAP[j];
-            get<1>(cur)
+            // T : <0> start, <1> end, <2> time
+            int s, e, t;
+            tie(s, e, t) = MAP[j];
+            if (TIME[s] == INF)
+                continue;
+            if (TIME[s] + t < TIME[e])
+                TIME[e] = TIME[s] + t;
         }
     }
-}
-void TimeInit(vector<int> &TIME)
-{
-    for (int i = 0; i < TIME.size(); i++)
-        TIME[i] = INF;
+    for (int i = 0; i < MAP.size(); i++)
+    {
+        int s, e, t;
+        tie(s, e, t) = MAP[i];
+        if (TIME[s] == INF)
+            continue;
+        if (TIME[s] + t < TIME[e])
+            return true; // time goes to minus forever(return to start point)
+    }
+    return false;
 }
