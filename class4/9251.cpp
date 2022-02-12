4ff49f7f7342d4043afbc31da4b95d5278d41efe
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-02-13 00:00:52 +0900

    LCS

--- class4/9251.cpp
+++ class4/9251.cpp
@@ -1,4 +1,32 @@
-// 9251 LCC
+// 9251 LCS
 #include <iostream>
 #include <vector>
+#include <string>
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
 using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    vector<vector<int>> table(str1.length() + 1, vector<int>(str2.length() + 1, 0));
+
+    for (int i = 1; i <= str1.length(); i++)
+    {
+        for (int j = 1; j <= str2.length(); j++)
+        {
+            if (str1[i - 1] == str2[j - 1])
+                table[i][j] = table[i - 1][j - 1] + 1;
+            else
+                table[i][j] = MAX(table[i - 1][j], table[i][j - 1]);
+        }
+    }
+
+    cout << table[str1.length()][str2.length()];
+    return 0;
+}
