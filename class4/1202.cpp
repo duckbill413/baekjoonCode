9ae474d6c9b38e3dd67c2c49310d04346e7d9b70
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-03-03 11:36:46 +0900

    보석 도둑

---
+++ class4/1202.cpp
@@ -0,0 +1,62 @@
+// 1202 보석 도둑
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+typedef struct jewel
+{
+    int m; // weight
+    int v; // price
+} Jewel;
+
+struct compare
+{
+    bool operator()(Jewel a, Jewel b)
+    {
+        return a.v < b.v;
+    }
+};
+
+bool findBag(int start, int end, int target, vector<int> &bag)
+{
+
+    int mid = (start + end) / 2;
+    if (bag[mid] == target)
+        return 1;
+    else if (bag[mid] < target)
+        return findBag(0, mid - 1, target, bag);
+    else
+        return findBag(mid + 1, end, target, bag);
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+    priority_queue<Jewel, vector<Jewel>, compare> q;
+    for (int i = 0; i < N; i++)
+    {
+        Jewel tmp;
+        cin >> tmp.m >> tmp.v;
+        q.push(tmp);
+    }
+    vector<int> bag(K, 0);
+    for (int i = 0; i < K; i++)
+        cin >> bag[i];
+    sort(bag.begin(), bag.end());
+
+    int total = 0;
+    for (int i = 0; i < K; i++)
+    {
+        Jewel target = q.top();
+        q.pop();
+
+        if (findBag(0, K - 1, target.m, bag))
+            total += target.v;
+    }
+    cout << total;
+}
